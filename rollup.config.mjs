fcde561f83dfcf64afea065096cf68d14846818a
Anton Savienko <asavienko@gmail.com>
2023-05-09 17:51:12 -0600

    new workaround

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,63 +1,40 @@
-import sucrase from "@rollup/plugin-sucrase";
-import resolve, { nodeResolve } from "@rollup/plugin-node-resolve";
-import typescript from "@rollup/plugin-typescript";
-import commonjs from "@rollup/plugin-commonjs";
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import { terser } from 'rollup-plugin-terser';
+import external from 'rollup-plugin-peer-deps-external';
+import postcss from 'rollup-plugin-postcss';
+import dts from 'rollup-plugin-dts';
 
-// To handle css files
-import postcss from "rollup-plugin-postcss";
-
-import { terser } from "rollup-plugin-terser";
-import peerDepsExternal from "rollup-plugin-peer-deps-external";
-import image from "@rollup/plugin-image";
-import dts from "rollup-plugin-dts";
-
-const config = [
-  {
-    input: `src/index.ts`,
-    ignore: ["node_modules/**"],
-    output: [
-      {
-        file: `./lib/bundle.js`,
-        format: "umd",
-        name: "MyLibrary",
-        esModule: false,
-        sourcemap: true,
-        strict: false,
-      },
-      {
-        file: `./lib/esm/index.js`,
-        format: "esm",
-        sourcemap: true,
-      },
-      {
-        file: `./lib/cjs/index.js`,
-        format: "cjs",
-        sourcemap: true,
-      },
-    ],
-    plugins: [
-      peerDepsExternal(),
-      resolve(),
-      commonjs(),
-      typescript({ tsconfig: "./tsconfig.lib.json" }),
-      postcss(),
-      terser(),
-      image(),
-      sucrase({
-        exclude: ["node_modules/**", "src/pages"],
-        transforms: ["typescript", "jsx", "imports"],
-      }),
-      nodeResolve({
-        extensions: [".js", ".jsx", ".ts", ".tsx"],
-      }),
-    ],
-  },
-  {
-    input: "lib/esm/types/index.d.ts",
-    output: [{ file: "lib/index.d.ts", format: "esm" }],
-    plugins: [dts()],
-
-    external: [/\.css$/], // telling rollup anything that is .css aren't part of type exports
-  },
-];
-export default config;
+export default [
+    {
+        input: 'src/index.ts',
+        output: [
+            {
+                file: "dist/cjs/index.js",
+                format: 'cjs',
+                sourcemap: true,
+                name: 'react-ts-lib'
+            },
+            {
+                file: "dist/esm/index.js",
+                format: 'esm',
+                sourcemap: true
+            }
+        ],
+        plugins: [
+            external(),
+            resolve(),
+            commonjs(),
+            typescript({ tsconfig: './tsconfig.json' }),
+            postcss(),
+            terser()
+        ],
+    },
+    {
+        input: 'dist/esm/types/index.d.ts',
+        output: [{ file: 'dist/index.d.ts', format: "esm" }],
+        external: [/\.css$/],
+        plugins: [dts()],
+    },
+]
