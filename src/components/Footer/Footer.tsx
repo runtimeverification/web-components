8cbcb1b8ec26b7c0c088304b89514cceb24f4217
Anton Savienko <asavienko@gmail.com>
2023-05-30 12:05:39 -0600

    make logo clickable

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,17 +5,19 @@ export default function Footer() {
   return (
     <footer className="footer items-center text-neutral-content mx-auto max-w-7xl p-4 px-2 sm:px-6 lg:px-8">
       <div className="flex flex-col items-center sm:items-start w-full">
-        <picture>
-          <source
-            srcSet="https://runtimeverification.com/assets/img/rv-logo-dark.png"
-            media="(prefers-color-scheme: dark)"
-          />
-          <img
-            className="h-[32px]"
-            src="https://runtimeverification.com/assets/img/rv-logo.png"
-            alt="Runtime Verification logo"
-          />
-        </picture>
+        <a href="https://runtimeverification.com">
+          <picture>
+            <source
+              srcSet="https://runtimeverification.com/assets/img/rv-logo-dark.png"
+              media="(prefers-color-scheme: dark)"
+            />
+            <img
+              className="h-[32px]"
+              src="https://runtimeverification.com/assets/img/rv-logo.png"
+              alt="Runtime Verification logo"
+            />
+          </picture>
+        </a>
         <p className="mt-2 text-md-left copyright link link-primary no-underline">
           <a href="https://goo.gl/maps/5iu2nhbUA48fs7fP6" target="_blank">
             <picture>
